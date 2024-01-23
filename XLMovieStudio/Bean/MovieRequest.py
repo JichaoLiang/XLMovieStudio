d58bcc7130cac4b672e762ae8502cc21fd2a14f8
liangjichao <liangjichao@58.com>
2024-01-23 17:27:08 +0800

    onboard new project

---
+++ XLMovieStudio/Bean/MovieRequest.py
@@ -0,0 +1,21 @@
+
+class Scence:
+    backgroundmusic = ""
+    pass
+
+class PicScence(Scence):
+
+    pass
+
+class MovieRequest:
+    scencelist = []
+    pass
+
+class scencenode:
+    offsetsec = 0
+    scene: Scence = None
+    pass
+
+
+class Effect:
+    pass
